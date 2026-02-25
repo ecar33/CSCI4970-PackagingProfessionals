57f437e14c4f6709d94e5e8a8d247e14f374e865
Evan C <evanc2022@gmail.com>
2026-02-24 18:35:33 -0600

    Link frontend and backend
    Invoking a flask endpoint from react
    
    Changed backend dockerfile

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentStatus, setCurrentStatus] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/health').then(res => res.json()).then(data => {
+      setCurrentStatus(data.status);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        ... no changes in this part ...
+
+        <p>The current status is: {currentStatus}.</p>
       </header>
     </div>
   );
