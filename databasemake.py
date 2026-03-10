79e965b29ee139278afa71955a64160cf79d8a36
T-Trojanowski <131211697+T-Trojanowski@users.noreply.github.com>
2026-03-10 18:26:25 -0500

    modified databasemake file added to branch not intended to merge

---
+++ databasemake.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import create_engine
+
+engine = create_engine("sqlite://", echo=True)
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+db.init_app(app)
+
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+class Inventory(db.Model):
+    __tablename__ = "inventory"
+    sku: Mapped[str] = mapped_column(primary_key=True)
+    Description: Mapped[str]
+    Item_Quantity: Mapped[int]
+    Return_Quantity: Mapped[int]
+
+
+
+with app.app_context():
+    db.create_all()
